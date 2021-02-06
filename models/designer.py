f0b8d86ed0286371e05c03da86661ec13f47e2c3
ShaharLutatiPersonal <shahar761@gmail.com>
2021-02-06 19:01:55 +0200

    add files v0'

---
+++ models/designer.py
@@ -0,0 +1,241 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import cost
+import resnet
+
+class designer_network(nn.Module):
+    def __init__(self,fc_shape):
+        super(designer_network,self).__init__()
+        self.hypernet = hypernetwork_constarints(fc_parameters=fc_shape)
+        self.relu = nn.ELU()
+        self.activation = nn.Sigmoid()
+        self.fc_shape = fc_shape
+    def forward(self,input):
+        cord,constraints = input
+        linear_weights = self.hypernet(constraints).squeeze()
+        tot_before = 0
+        output = cord
+        for i,fc in enumerate(self.fc_shape):
+            shape = fc['shape']
+            h,w = shape[0],shape[1]
+            lin = linear_weights[tot_before:tot_before+h*w+2*w]
+            linear = lin[:h*w].view(w,h)
+            bias = lin[h*w:h*w+w]
+            scale = lin[h*w+w:h*w+2*w]
+            tot_before += h*w+w
+            if i == (len(self.fc_shape) -1):
+                output = self.scaled_linear(w = linear,s = scale,b = bias,input = output)
+            else:
+                output = self.relu(self.scaled_linear(w = linear,s = scale,b = bias,input =output))
+        return output
+    def scaled_linear(self,w,s,b,input):
+        input = input.matmul(w.t())
+        input = input * s + b
+        return input
+
+
+
+
+class designer_fine_tuned_network(nn.Module):
+    def __init__(self,fc_shape):
+        super(designer_fine_tuned_network,self).__init__()
+        self.hypernet = hypernetwork_constarints(fc_parameters=fc_shape)
+        self.hyper_fine = fine_tuner_hyper(fc_parameters=fc_shape)
+        self.relu = nn.ELU()
+        self.activation = nn.Sigmoid()
+        self.fc_shape = fc_shape
+    def forward(self,input):
+        (cord,constraints),metal_plane = input
+        linear_weights = self.hyper_fine(self.hypernet(constraints),metal_plane).squeeze()
+        tot_before = 0
+        output = cord
+        for i,fc in enumerate(self.fc_shape):
+            shape = fc['shape']
+            h,w = shape[0],shape[1]
+            lin = linear_weights[tot_before:tot_before+h*w+2*w]
+            linear = lin[:h*w].view(w,h)
+            bias = lin[h*w:h*w+w]
+            scale = lin[h*w+w:h*w+2*w]
+            tot_before += h*w+w
+            if i == (len(self.fc_shape) -1):
+                output = self.scaled_linear(w = linear,s = scale,b = bias,input = output)
+            else:
+                output = self.relu(self.scaled_linear(w = linear,s = scale,b = bias,input =output))
+        return output
+    def scaled_linear(self,w,s,b,input):
+        input = input.matmul(w.t())
+        input = input * s + b
+        return input
+    
+    def load_my_state_dict(self, state_dict):
+        own_state = self.state_dict()
+        for name, param in state_dict.items():
+            if name not in own_state:
+                    continue
+            if isinstance(param, torch.nn.Parameter):
+                # backwards compatibility for serialized parameters
+                param = param.data
+            own_state[name].copy_(param)
+
+
+
+
+
+
+
+
+
+
+import resnet
+class hypernetwork_constarints(nn.Module):
+    def __init__(self,fc_parameters):
+        super(hypernetwork_constarints,self).__init__()
+        
+        self.relu = nn.ELU()
+        self.directivity_conv = nn.Sequential( resnet.ResNetBasicBlock(1,16),nn.MaxPool2d(2), resnet.ResNetBasicBlock(16,16),
+        resnet.ResNetBasicBlock(16,16), resnet.ResNetBasicBlock(16,1))
+        self.fc = nn.Linear(1024,1024)
+        cnt = 0
+        for fc in fc_parameters:
+            shape = fc['shape']
+            cnt += shape[0]*shape[1] + 2*shape[1]
+        
+        self.fc2 = nn.Linear(1024,cnt)
+        self.init_weight_network(self.fc2.weight,64)
+        #nn.init.kaiming_normal_(self.fc2.weight)
+        nn.init.kaiming_normal_(self.fc.weight)
+        #self.metal_plane_conv = nn.Sequential(conv_block([16,32,3]),nn.MaxPool2d(2),conv_block([32,16,3]),conv_block([16,8,3]),nn.MaxPool2d(2),conv_block([8,1,3]))
+        # size of metal plane = 121
+
+        
+    def forward(self,input):
+        directivity = input
+        directivity = self.directivity_conv(directivity)
+        #metal_plane = self.metal_plane_conv(metal_plane)
+        #concat = torch.cat((directivity.view(1,-1),metal_plane.view(1,-1)),dim = 1)
+        concat = directivity.view(1,-1)
+        concat = self.relu(self.fc(concat))
+        concat = self.relu(self.fc2(concat))
+        return concat
+ 
+    def init_bias_network(self,w):
+        with torch.no_grad():
+            fan_in = w.shape[0]
+            uni = torch.randn(w.shape)
+            a = torch.sqrt(torch.tensor(1/fan_in))
+            w = uni*a
+            return w    
+
+    def init_weight_network(self,w,fan_out_dj):
+        with torch.no_grad():
+            fan_in = w.shape[0]
+            a = torch.sqrt(torch.tensor(3/(fan_in*fan_out_dj)))
+            return w.uniform_(-a,a)
+
+class conv_block(nn.Module):
+    def __init__(self,init_shape):
+        super(conv_block,self).__init__()
+        self.conv0 = nn.Conv2d(in_channels=init_shape[0],out_channels=init_shape[1],kernel_size=init_shape[2])
+        self.relu = nn.ELU()
+        nn.init.kaiming_uniform_(self.conv0.weight)
+    def forward(self,input):
+        out = self.relu(self.conv0(input))
+        return out
+    
+
+class fc_block(nn.Module):
+    def __init__(self,start,init_shape):
+        super(fc_block,self).__init__()
+        shape = init_shape['shape']
+        self.fc = nn.Linear(in_features=start,out_features=shape[0]*shape[1] + 2*shape[1])
+        self.relu = nn.ELU()
+        self.init_weight_network(self.fc.weight,shape[1])
+    def forward(self,input):
+        out = self.relu(self.fc(input))
+        return out
+
+    def init_weight_network(self,w,fan_out_dj):
+        with torch.no_grad():
+            fan_in = w.shape[0]
+            a = torch.sqrt(torch.tensor(3/(fan_in*fan_out_dj)))
+            return w.uniform_(-a,a)
+
+
+
+class multiloss(nn.Module):
+    def __init__(self,objective_num):
+        super(multiloss,self).__init__()
+        self.objective_num = objective_num
+        self.log_var = nn.Parameter(torch.zeros(self.objective_num))
+    
+    def forward(self,losses):
+        for i in range(len(losses)):
+            precision = torch.exp(-self.log_var[i])
+            if i == 0:
+                loss = precision*losses[i] + self.log_var[i]
+            else :
+                loss += precision*losses[i] + self.log_var[i]
+        return loss
+
+
+class fine_tuner_hyper(nn.Module):
+    def __init__(self,fc_parameters):
+        super(fine_tuner_hyper,self).__init__()
+        cnt = 0
+        for fc in fc_parameters:
+            shape = fc['shape']
+            cnt += shape[0]*shape[1] + 2*shape[1]
+        self.metal_plane_feature_map = nn.Sequential(nn.Conv2d(16,1,3),nn.ELU(),nn.Conv2d(1,1,3),nn.ELU(),nn.Conv2d(1,1,3))
+        self.fc = nn.Linear(3364,cnt)
+        self.fc2 = nn.Linear(2*cnt,cnt)
+        self.activation = nn.ELU()
+        nn.init.kaiming_uniform(self.fc.weight)
+        self.init_weight_network(self.fc2.weight,32)
+    def forward(self,weights,mp):
+        mp = self.metal_plane_feature_map(mp).squeeze().view(1,-1)
+        w = self.activation(self.fc2(torch.cat((weights,self.fc(self.activation(mp))),dim= 1)))
+        return w
+
+    def init_weight_network(self,w,fan_out_dj):
+        with torch.no_grad():
+            fan_in = w.shape[0]
+            a = torch.sqrt(torch.tensor(3/(fan_in*fan_out_dj)))
+            return w.uniform_(-a,a)
+
+
+
+class fine_tuner(nn.Module):
+    def __init__(self):
+        super(fine_tuner,self).__init__()
+        self.metal_plane_feature_map = nn.Sequential( resnet.ResNetBasicBlock(16,16), resnet.ResNetBasicBlock(16,16),
+        resnet.ResNetBasicBlock(16,16))
+        self.dec_cube_feature_map = nn.Sequential( resnet.ResNetBasicBlock(16,16), resnet.ResNetBasicBlock(16,16),
+        resnet.ResNetBasicBlock(16,16))
+        self.combining = resnet.ResNetBasicBlock(in_channels = 32,out_channels = 16)
+        self.weights = nn.Parameter(torch.ones(2))
+        self.activation = nn.Identity() 
+    def forward(self,decision_cube,metal_plane):
+        mp = self.metal_plane_feature_map(metal_plane)
+        dc = self.dec_cube_feature_map(decision_cube)
+        combined = self.combining(torch.cat((mp,dc),dim=1))
+        #print(mp.shape)
+        #print(di.shape)
+        #print(combined.shape)
+        output = self.activation(self.weights[0]*combined+ self.weights[1]*decision_cube)
+        return output
+
+class baseline_network(nn.Module):
+    def __init__(self):
+        super(baseline_network,self).__init__()
+        self.directivity_conv = nn.Sequential( resnet.ResNetBasicBlock(1,16), resnet.ResNetBasicBlock(16,16),
+        resnet.ResNetBasicBlock(16,16),resnet.ResNetBasicBlock(16,16),resnet.ResNetBasicBlock(16,16), resnet.ResNetBasicBlock(16,16))
+        #self.weight_2 = nn.Parameter(torch.ones(64*64*16))
+        #self.bias_2 = nn.Parameter(torch.zeros(64*64*16))
+        #self.activation = nn.ELU()
+        self.sigmoid = nn.Sigmoid()
+    def forward(self,directivity):
+        dec = self.directivity_conv(directivity)
+        #dec = self.sigmoid((dec.view(-1)*self.weight_2) + self.bias_2)
+        output = self.sigmoid(dec.view(1,16,64,64))
+        return output
---
+++ training/array_training.py
@@ -0,0 +1,279 @@
+import torch
+import torch.nn as nn
+import array_network_resnet  
+import numpy as np
+from scipy.io import loadmat
+import ctypes
+from random import shuffle
+import matplotlib.pyplot as plt
+import pytorch_msssim
+from PIL import Image
+import simulator_network
+import matplotlib.pyplot as plt
+ctypes.cdll.LoadLibrary('caffe2_nvrtc.dll')
+torch.cuda.empty_cache()
+device = torch.device('cuda')
+
+
+
+# Some initial computation 
+ssim =  pytorch_msssim.ms_ssim
+theta_range = np.linspace(0,np.pi/2,64)
+phi_range = np.linspace(0,np.pi,64)
+sincos = np.outer(np.sin(theta_range),np.cos(phi_range))
+sinsin = np.outer(np.sin(theta_range),np.sin(phi_range))
+p = np.array([[0,0],[1,0],[1,1],[0,1],[2,0],[2,1]])*2*0.45*np.pi
+for i in range(6):
+    tmp = np.exp(-1j*(sincos*p[i,0] + sinsin*p[i,1]))
+    if i == 0:
+        factor = tmp.reshape(1,64,64)
+    else:
+        factor = np.concatenate([factor,tmp.reshape(1,64,64)],0)
+
+array_factor = torch.tensor(factor).to('cuda')
+
+
+
+
+import utils
+def show_image(x):
+    fig,ax = plt.subplots()
+    ax.imshow(x)
+    fig.show()
+
+def imresize(x,h,w):
+    return np.array(Image.fromarray(x).resize(size=(h, w)))
+
+import torchvision.transforms.functional as TF
+from torchvision import transforms
+
+
+def extract_single_antenna(G,L,cnt_len,i):
+    if i == 0:
+        prev = 0
+    else:
+        prev = sum(cnt_len[:i])
+    return G[prev:prev + cnt_len[i],],L[prev:prev + cnt_len[i],]
+
+assigment_pos = [[0,0],[1,0],[1,1],[0,1],[2,0],[2,1]]
+
+def gen_metal_plane(pos):
+    x = torch.zeros(1,64*3,64*2)
+    for i in pos:
+        x[0,assigment_pos[i][0]*64:assigment_pos[i][0]*64 + 64,assigment_pos[i][1]*64:assigment_pos[i][1]*64 + 64] = 1
+    return x
+
+def fix_L(label,coord,ranges,random_block_metal,index):
+    co_x = coord[:,0].cpu().numpy() / 1000.0
+    co_y = coord[:,1].cpu().numpy() / 1000.0
+    ranges = ranges.cpu().numpy()
+    mp = random_block_metal[0,index*64:(index+1)*64,index*64:(index+1)*64].cpu().numpy()
+    for i in range(len(label)):
+        x_ix = int(np.round((co_x[i] - ranges[0][0][0]) / ((ranges[0][0][1]-ranges[0][0][0])/63)))
+        y_ix = int(np.round((co_y[i] - ranges[0][1][0]) / ((ranges[0][1][1]-ranges[0][1][0])/63)))
+        if mp[x_ix,y_ix] == 1:
+            label[i] = 0
+    return label
+
+
+
+class multiloss(nn.Module):
+    def __init__(self,objective_num):
+        super(multiloss,self).__init__()
+        self.objective_num = objective_num
+        self.log_var = nn.Parameter(torch.zeros(self.objective_num))
+    
+    def forward(self,losses):
+        for i in range(len(losses)):
+            precision = torch.exp(-self.log_var[i])
+            if i == 0:
+                loss = precision*losses[i] + self.log_var[i]
+            else :
+                loss += precision*losses[i] + self.log_var[i]
+        return loss
+
+
+def gen_voxel_from_points_tensor(cord,points,labels):
+    voxel = torch.zeros(16,64,64).to(device)
+    cord = cord.cpu().numpy()
+    points = points.cpu().numpy()
+    res = labels
+    z_arr = points[:,2]
+    x_arr = points[:,0]
+    y_arr = points[:,1]
+    z_arr =np.clip((np.round((z_arr - cord[0][2][0])/((cord[0][2][1]-cord[0][2][0])/15))),0,15)
+    y_arr = np.clip((np.round((y_arr - cord[0][1][0])/((cord[0][1][1]-cord[0][1][0])/63))),0,63)
+    x_arr = np.clip((np.round((x_arr - cord[0][0][0])/((cord[0][0][1]-cord[0][0][0])/63))),0,63)
+    voxel[z_arr,x_arr,y_arr] = res
+    return voxel
+
+def simulator_wrapper(labels,coord,ranges,cnt_len):
+    for i in range(6):
+        G,o = extract_single_antenna(coord,labels,cnt_len,i)
+        fixed_range = fix_ranges(ranges[i],G/1000.0)
+        voxel = (gen_voxel_from_points_tensor(fixed_range*1000,G,o)).to('cuda')
+        rr = fixed_range.to('cuda')
+        sim_out_res = sim_net(((voxel*120).squeeze().permute(2,1,0).clamp_max_(100).unsqueeze(0),rr)).squeeze()
+        #sim_out_res = sim_out_res.to('cuda')
+        if i == 0:
+            R = sim_out_res.unsqueeze(0)
+        else:
+            R = torch.cat((R,sim_out_res.unsqueeze(0)),dim = 0)         
+    return (utils.array_gain_calc_torch(R.to('cuda'),array_factor)).unsqueeze(0).unsqueeze(0)
+
+def fix_ranges(r,g):
+    x_max = torch.max(g[:,0])
+    y_max = torch.max(g[:,1])
+    z_max = torch.max(g[:,2])
+    x_min = torch.min(g[:,0])
+    y_min = torch.min(g[:,1])
+    z_min = torch.min(g[:,2])
+    r[0][0][1] = torch.max(torch.tensor([r[0][0][1],x_max]))
+    r[0][1][1] = torch.max(torch.tensor([r[0][1][1],y_max]))
+    r[0][2][1] = torch.max(torch.tensor([r[0][2][1],z_max]))
+    r[0][0][0] = torch.min(torch.tensor([r[0][0][1],x_min]))
+    r[0][1][0] = torch.min(torch.tensor([r[0][1][1],y_min]))
+    r[0][2][0] = torch.min(torch.tensor([r[0][2][1],z_min]))
+    return r
+
+def constraint_loss(labels,coord,ranges_tmp,avoid_metal,cnt_len):
+    res = 0
+    for i in range(6):
+        ranges = ranges_tmp[i].cpu().numpy()
+        if i == 0:
+            prev = 0
+        else:
+            prev = sum(cnt_len[:i])
+        L = labels[prev:prev + cnt_len[i],]
+        cordi = coord[prev:prev + cnt_len[i],]
+        ranges = fix_ranges(ranges,cordi/1000.0)
+        co_x = cordi[:,0].cpu().numpy() / 1000.0
+        co_y = cordi[:,1].cpu().numpy() / 1000.0
+        mp = avoid_metal[0,i*64:(i+1)*64,i*64:(i+1)*64].cpu().numpy()
+        ix = np.round((co_x - ranges[0][0][0]) / ((ranges[0][0][1]-ranges[0][0][0])/63))
+        iy = np.round((co_y - ranges[0][1][0]) / ((ranges[0][1][1]-ranges[0][1][0])/63))
+        idx = torch.tensor([1 if mp[int(xx),int(yy)] else 0 for xx,yy in zip(ix,iy)]).to(device)
+        res += torch.mean(clap_log(L)*idx)
+    return res
+
+def clapped_log(x):
+    return torch.clip(10*torch.log10(x+1e-10),-100,10)
+
+
+import torchvision.transforms.functional as TF
+from torchvision import transforms
+
+def confusion(prediction, truth):
+    """ Returns the confusion matrix for the values in the `prediction` and `truth`
+    tensors, i.e. the amount of positions where the values of `prediction`
+    and `truth` are
+    - 1 and 1 (True Positive)
+    - 1 and 0 (False Positive)
+    - 0 and 0 (True Negative)
+    - 0 and 1 (False Negative)
+    """
+
+    confusion_vector = prediction / truth
+    # Element-wise division of the 2 tensors returns a new tensor which holds a
+    # unique value for each case:
+    #   1     where prediction and truth are 1 (True Positive)
+    #   inf   where prediction is 1 and truth is 0 (False Positive)
+    #   nan   where prediction and truth are 0 (True Negative)
+    #   0     where prediction is 0 and truth is 1 (False Negative)
+
+    true_positives = torch.sum(confusion_vector == 1).item()
+    false_positives = torch.sum(confusion_vector == float('inf')).item()
+    true_negatives = torch.sum(torch.isnan(confusion_vector)).item()
+    false_negatives = torch.sum(confusion_vector == 0).item()
+
+    return true_positives, false_positives, true_negatives, false_negatives
+
+class MyRotationTransform:
+    """Rotate by one of the given angles."""
+
+    def __init__(self):
+        self.angles = []
+        self.totensor = transforms.ToTensor()
+
+    def __call__(self, x,angle):
+        x = transforms.ToPILImage()(x)
+        x.rotate(angle)
+        return self.totensor(x)
+
+
+model = array_network_resnet.resnet_hyperhyper_variant().to(device).float()
+lr = float(input('Learning rate?\n'))
+l1 = nn.L1Loss()
+clip_norm = 1
+num_epochs = int(input('How many epochs?\n'))
+crossentropyloss = nn.CrossEntropyLoss(weight=torch.tensor([1,6]).float().to(device))
+decay_factor = float(input('Decay factor?\n'))
+l = 0
+p = torch.tensor(np.array([[0,0,0],[1,0,0],[1,1,0],[0,1,0],[2,0,0],[2,1,0]])*1000*0.45*3e8/2.4e9)
+def half_bce(source,target):
+    loss = -(source)*clapped_log(1-target)
+    loss = torch.mean(loss)
+    return loss
+
+
+obceloss = half_bce
+sigmoid = torch.sigmoid
+multiloss_func = multiloss(2).to(device)
+optim = torch.optim.Adam(model.parameters(),lr)
+inputs_path = ''
+Awesome_list =   torch.load(inputs_path) 
+n_examples = int(len(Awesome_list)*3/4)
+grad_accum = int(input('How many examples in minibatch?\n')) # equivalent to minibatch in term of loss averaging.
+import time
+loss_array_logger = []
+for epoch in range(num_epochs):
+    cnt = 0
+    cum_error = 0
+    optim.zero_grad()
+    cnt_avg_p = 0 
+    cnt_avg_n = 0
+    true_positives_avg = 0 
+    false_positives_avg = 0 
+    true_negatives_avg = 0 
+    false_negatives_avg = 0 
+    for nexp in range(n_examples):
+        #t_s = time.time()
+        g_input,G,L,random_block_metal,ranges_tmp,cnt_len,s = Awesome_list[nexp]
+        g_input = g_input.to(device)
+        G = G.to(device)
+        L = L.to(device)
+        random_block_metal = random_block_metal.to(device)   
+        #t_inputs = time.time()-t_s
+        output = model(g_input.to(device),G.float(),random_block_metal.unsqueeze(0).to(device),s.to(device)).squeeze()
+        fake_antenna = torch.ones(output.shape[0])
+        fake_antenna[sum(cnt_len):] = 0 
+        celit = crossentropyloss(output[:,:2],L.long())
+        logist_loss = obceloss(fake_antenna.to(device),sigmoid(output[:,2]))
+        l =  multiloss_func([celit,logist_loss])
+        loss_f = l
+        loss_array_logger.append(loss_f.item())
+        loss_f = loss_f/grad_accum
+        loss_f.backward()
+        torch.nn.utils.clip_grad_norm_(
+                        model.parameters(), clip_norm )
+        if (nexp+1) % grad_accum == 0:
+            optim.step()
+            model.zero_grad()
+        
+        with torch.no_grad():
+            out_dec = output[:,1] > output[:,0]
+            label = L
+            true_positives, false_positives, true_negatives, false_negatives = confusion(out_dec, label)
+            cnt_avg_p += torch.sum(label).item()
+            cnt_avg_n += label.shape[0] - torch.sum(label).item()
+            true_positives_avg += true_positives
+            false_positives_avg += false_positives
+            true_negatives_avg += true_negatives
+            false_negatives_avg += false_negatives
+
+        cnt += 1
+        if cnt % 100 == 0 :
+            print('epoch {} , {}/{} , cum_error (so far) {} , tp {} fn {} tn {} fp {}'.format(epoch,cnt,n_examples,cum_error/cnt,
+            true_positives_avg/cnt_avg_p,false_negatives_avg/cnt_avg_p,true_negatives_avg/cnt_avg_n,false_positives_avg/cnt_avg_n))
+    optim.param_groups[0]['lr'] *= decay_factor
+
